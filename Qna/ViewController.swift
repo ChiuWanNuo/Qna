ceeadb658181f38caa772abaf864f60c897422b2
Wan-Nuo, Chiu <sunnusbi@gmail.com>
2019-11-18 12:43:47 +0800

    Initial Commit

---
+++ Qna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qna
+//
+//  Created by ChiuWanNuo on 2019/11/18.
+//  Copyright © 2019 ChiuWanNuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
